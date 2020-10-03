6faef2caacfba3e683f159bca06693948a378648
Christian Winkler <christian.winkler@cwkr.de>
2020-10-03 18:24:47 +0200

    Fix PMD warning

--- src/main/java/de/cwkr/util/Errors.java
+++ src/main/java/de/cwkr/util/Errors.java
@@ -39,7 +39,7 @@ import org.slf4j.helpers.MessageFormatter;
  * @author Christian Winkler
  */
 public final class Errors implements Iterable<String> {
-    private static final Logger LOGGER = LoggerFactory.getLogger(Errors.class);
+    private static final Logger logger = LoggerFactory.getLogger(Errors.class);
     private final List<String> errors = new CopyOnWriteArrayList<>();
 
     public Errors() {
@@ -50,58 +50,58 @@ public final class Errors implements Iterable<String> {
     }
 
     public List<String> getErrors() {
-        LOGGER.trace("getErrors()");
+        logger.trace("getErrors()");
         return unmodifiableList(errors);
     }
 
     public void reset() {
-        LOGGER.trace("reset()");
+        logger.trace("reset()");
         errors.clear();
     }
 
     public void addError(String error) {
-        LOGGER.trace("addError(error = {})", error);
+        logger.trace("addError(error = {})", error);
         errors.add(error);
     }
 
     public void addErrors(Iterable<String> errors) {
-        LOGGER.trace("addErrors(errors = {})", errors);
+        logger.trace("addErrors(errors = {})", errors);
         Objects.requireNonNull(errors, "errors must not be null");
         errors.forEach(this::addError);
     }
 
     public void addErrors(String... errors) {
-        if(LOGGER.isTraceEnabled()) {
-            LOGGER.trace("addErrors(errors = {})", Arrays.toString(errors));
+        if(logger.isTraceEnabled()) {
+            logger.trace("addErrors(errors = {})", Arrays.toString(errors));
         }
         Objects.requireNonNull(errors, "errors must not be null");
         Arrays.stream(errors).forEach(this::addError);
     }
 
     public int countErrors() {
-        LOGGER.trace("countErrors()");
+        logger.trace("countErrors()");
         return errors.size();
     }
 
     public boolean hasErrors() {
-        LOGGER.trace("hasErrors()");
+        logger.trace("hasErrors()");
         return !errors.isEmpty();
     }
 
     public void logErrors(Logger logger) {
-        LOGGER.trace("logErrors(logger = {})", logger);
+        Errors.logger.trace("logErrors(logger = {})", logger);
         Objects.requireNonNull(logger, "logger must not be null");
         errors.forEach(logger::error);
     }
 
     public void logErrors() {
-        LOGGER.trace("logErrors()");
-        logErrors(LOGGER);
+        logger.trace("logErrors()");
+        logErrors(logger);
     }
 
     public void throwErrors(ExceptionProducer<? extends RuntimeException> exceptionProducer, String msg, Object ...params) {
-        if(LOGGER.isTraceEnabled()) {
-            LOGGER.trace("throwErrors(exceptionProducer = {}, msg = {}, params = {})", exceptionProducer, msg, Arrays.toString(params));
+        if(logger.isTraceEnabled()) {
+            logger.trace("throwErrors(exceptionProducer = {}, msg = {}, params = {})", exceptionProducer, msg, Arrays.toString(params));
         }
         if(hasErrors()) {
             throw exceptionProducer.produce(
@@ -113,13 +113,13 @@ public final class Errors implements Iterable<String> {
 
     @Override
     public Iterator<String> iterator() {
-        LOGGER.trace("iterator()");
+        logger.trace("iterator()");
         return unmodifiableIterator(errors.iterator());
     }
 
     @Override
     public String toString() {
-        LOGGER.trace("toString()");
+        logger.trace("toString()");
         return new ToStringBuilder(this).append("errors", errors)
                                         .build();
     }
@@ -145,8 +145,8 @@ public final class Errors implements Iterable<String> {
     }
 
     public void isNull(Object obj, String msg, Object ...params) {
-        if(LOGGER.isTraceEnabled()) {
-            LOGGER.trace("isNull(obj = {}, msg = {}, params = {})", obj, msg, Arrays.toString(params));
+        if(logger.isTraceEnabled()) {
+            logger.trace("isNull(obj = {}, msg = {}, params = {})", obj, msg, Arrays.toString(params));
         }
         if(!Objects.isNull(obj)) {
             errors.add(MessageFormatter.arrayFormat(msg, params).getMessage());
@@ -154,8 +154,8 @@ public final class Errors implements Iterable<String> {
     }
 
     public void isNotNull(Object obj, String msg, Object ...params) {
-        if(LOGGER.isTraceEnabled()) {
-            LOGGER.trace("isNotNull(obj = {}, msg = {}, params = {})", obj, msg, Arrays.toString(params));
+        if(logger.isTraceEnabled()) {
+            logger.trace("isNotNull(obj = {}, msg = {}, params = {})", obj, msg, Arrays.toString(params));
         }
         if(Objects.isNull(obj)) {
             errors.add(MessageFormatter.arrayFormat(msg, params).getMessage());
@@ -163,8 +163,8 @@ public final class Errors implements Iterable<String> {
     }
 
     public void isEmpty(CharSequence obj, String msg, Object ...params) {
-        if(LOGGER.isTraceEnabled()) {
-            LOGGER.trace("isEmpty(obj = {}, msg = {}, params = {})", obj, msg, Arrays.toString(params));
+        if(logger.isTraceEnabled()) {
+            logger.trace("isEmpty(obj = {}, msg = {}, params = {})", obj, msg, Arrays.toString(params));
         }
         if(StringUtils.isNotEmpty(obj)) {
             errors.add(MessageFormatter.arrayFormat(msg, params).getMessage());
@@ -172,8 +172,8 @@ public final class Errors implements Iterable<String> {
     }
 
     public void isNotEmpty(CharSequence obj, String msg, Object ...params) {
-        if(LOGGER.isTraceEnabled()) {
-            LOGGER.trace("isNotEmpty(obj = {}, msg = {}, params = {})", obj, msg, Arrays.toString(params));
+        if(logger.isTraceEnabled()) {
+            logger.trace("isNotEmpty(obj = {}, msg = {}, params = {})", obj, msg, Arrays.toString(params));
         }
         if(StringUtils.isEmpty(obj)) {
             errors.add(MessageFormatter.arrayFormat(msg, params).getMessage());
@@ -181,8 +181,8 @@ public final class Errors implements Iterable<String> {
     }
 
     public void isEmpty(Collection<?> obj, String msg, Object ...params) {
-        if(LOGGER.isTraceEnabled()) {
-            LOGGER.trace("isEmpty(obj = {}, msg = {}, params = {})", obj, msg, Arrays.toString(params));
+        if(logger.isTraceEnabled()) {
+            logger.trace("isEmpty(obj = {}, msg = {}, params = {})", obj, msg, Arrays.toString(params));
         }
         if(!obj.isEmpty()) {
             errors.add(MessageFormatter.arrayFormat(msg, params).getMessage());
@@ -190,8 +190,8 @@ public final class Errors implements Iterable<String> {
     }
 
     public void isNotEmpty(Collection<?> obj, String msg, Object ...params) {
-        if(LOGGER.isTraceEnabled()) {
-            LOGGER.trace("isNotEmpty(obj = {}, msg = {}, params = {})", obj, msg, Arrays.toString(params));
+        if(logger.isTraceEnabled()) {
+            logger.trace("isNotEmpty(obj = {}, msg = {}, params = {})", obj, msg, Arrays.toString(params));
         }
         if(obj.isEmpty()) {
             errors.add(MessageFormatter.arrayFormat(msg, params).getMessage());
@@ -199,8 +199,8 @@ public final class Errors implements Iterable<String> {
     }
 
     public void isBlank(CharSequence obj, String msg, Object ...params) {
-        if(LOGGER.isTraceEnabled()) {
-            LOGGER.trace("isBlank(obj = {}, msg = {}, params = {})", obj, msg, Arrays.toString(params));
+        if(logger.isTraceEnabled()) {
+            logger.trace("isBlank(obj = {}, msg = {}, params = {})", obj, msg, Arrays.toString(params));
         }
         if(StringUtils.isNotBlank(obj)) {
             errors.add(MessageFormatter.arrayFormat(msg, params).getMessage());
@@ -208,8 +208,8 @@ public final class Errors implements Iterable<String> {
     }
 
     public void isNotBlank(CharSequence obj, String msg, Object ...params) {
-        if(LOGGER.isTraceEnabled()) {
-            LOGGER.trace("isNotBlank(obj = {}, msg = {}, params = {})", obj, msg, Arrays.toString(params));
+        if(logger.isTraceEnabled()) {
+            logger.trace("isNotBlank(obj = {}, msg = {}, params = {})", obj, msg, Arrays.toString(params));
         }
         if(StringUtils.isBlank(obj)) {
             errors.add(MessageFormatter.arrayFormat(msg, params).getMessage());
@@ -217,8 +217,8 @@ public final class Errors implements Iterable<String> {
     }
 
     public void isContainingDuplicates(Collection<?> obj, String msg, Object ...params) {
-        if(LOGGER.isTraceEnabled()) {
-            LOGGER.trace("isContainingDuplicates(obj = {}, msg = {}, params = {})", obj, msg, Arrays.toString(params));
+        if(logger.isTraceEnabled()) {
+            logger.trace("isContainingDuplicates(obj = {}, msg = {}, params = {})", obj, msg, Arrays.toString(params));
         }
         if(obj.stream().noneMatch(e -> Collections.frequency(obj, e) > 1)) {
             errors.add(MessageFormatter.arrayFormat(msg, params).getMessage());
@@ -226,8 +226,8 @@ public final class Errors implements Iterable<String> {
     }
 
     public void isNotContainingDuplicates(Collection<?> obj, String msg, Object ...params) {
-        if(LOGGER.isTraceEnabled()) {
-            LOGGER.trace("isNotContainingDuplicates(obj = {}, msg = {}, params = {})", obj, msg, Arrays.toString(params));
+        if(logger.isTraceEnabled()) {
+            logger.trace("isNotContainingDuplicates(obj = {}, msg = {}, params = {})", obj, msg, Arrays.toString(params));
         }
         if(obj.stream().anyMatch(e -> Collections.frequency(obj, e) > 1)) {
             errors.add(MessageFormatter.arrayFormat(msg, params).getMessage());
@@ -235,8 +235,8 @@ public final class Errors implements Iterable<String> {
     }
 
     public void isBetween(int num, int min, int max, String msg, Object ...params) {
-        if(LOGGER.isTraceEnabled()) {
-            LOGGER.trace("isBetween(num = {}, min = {}, max = {}, msg = {}, params = {})", num, min, max, msg, Arrays.toString(params));
+        if(logger.isTraceEnabled()) {
+            logger.trace("isBetween(num = {}, min = {}, max = {}, msg = {}, params = {})", num, min, max, msg, Arrays.toString(params));
         }
         if(num < min || num > max) {
             errors.add(MessageFormatter.arrayFormat(msg, params).getMessage());
@@ -244,8 +244,8 @@ public final class Errors implements Iterable<String> {
     }
 
     public void isTrue(boolean obj, String msg, Object ...params) {
-        if(LOGGER.isTraceEnabled()) {
-            LOGGER.trace("isTrue(obj = {}, msg = {}, params = {})", obj, msg, Arrays.toString(params));
+        if(logger.isTraceEnabled()) {
+            logger.trace("isTrue(obj = {}, msg = {}, params = {})", obj, msg, Arrays.toString(params));
         }
         if(!obj) {
             errors.add(MessageFormatter.arrayFormat(msg, params).getMessage());
@@ -253,8 +253,8 @@ public final class Errors implements Iterable<String> {
     }
 
     public void isFalse(boolean obj, String msg, Object ...params) {
-        if(LOGGER.isTraceEnabled()) {
-            LOGGER.trace("isFalse(obj = {}, msg = {}, params = {})", obj, msg, Arrays.toString(params));
+        if(logger.isTraceEnabled()) {
+            logger.trace("isFalse(obj = {}, msg = {}, params = {})", obj, msg, Arrays.toString(params));
         }
         if(obj) {
             errors.add(MessageFormatter.arrayFormat(msg, params).getMessage());
@@ -262,8 +262,8 @@ public final class Errors implements Iterable<String> {
     }
 
     public void isEqual(Object obj, Object other, String msg, Object ...params) {
-        if(LOGGER.isTraceEnabled()) {
-            LOGGER.trace("isEqual(obj = {}, other = {}, msg = {}, params = {})", obj, other, msg, Arrays.toString(params));
+        if(logger.isTraceEnabled()) {
+            logger.trace("isEqual(obj = {}, other = {}, msg = {}, params = {})", obj, other, msg, Arrays.toString(params));
         }
         if(!Objects.equals(obj, other)) {
             errors.add(MessageFormatter.arrayFormat(msg, params).getMessage());
@@ -271,8 +271,8 @@ public final class Errors implements Iterable<String> {
     }
 
     public void isNotEqual(Object obj, Object other, String msg, Object ...params) {
-        if(LOGGER.isTraceEnabled()) {
-            LOGGER.trace("isNotEqual(obj = {}, other = {}, msg = {}, params = {})", obj, other, msg, Arrays.toString(params));
+        if(logger.isTraceEnabled()) {
+            logger.trace("isNotEqual(obj = {}, other = {}, msg = {}, params = {})", obj, other, msg, Arrays.toString(params));
         }
         if(Objects.equals(obj, other)) {
             errors.add(MessageFormatter.arrayFormat(msg, params).getMessage());
